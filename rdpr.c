7431aa5655c5a40fecdd14ed2d5e4b8521a5bc7b
Nims09 <nmots@live.ca>
2014-11-04 13:31:28 -0800

    started working on packet buildind

--- rdpr.c
+++ rdpr.c
@@ -17,12 +17,23 @@
 #include <errno.h>
 #include <time.h> 
 
+#define MAXPACKETSIZE 1024
 #define MAXBUFLEN 256
+
+// Set portNo on an addr
+struct sockaddr_in setAddressAndPortNo(char *addr, int portno);
+
+// Send a string to the client
+int sendString(int sockfd, char *buffer, struct sockaddr_in addr, socklen_t len);
+
+// Confirm the file exists
+int checkFilePath(char *loc);
 
 int main(int argc, char *argv[]) {
 	int sockfd;
 	int portno;
 	char *address;
+  char *file_path;  
 	struct sockaddr_in recv_addr, sender_addr;
   socklen_t sender_len;
   char buffer[MAXBUFLEN];   
@@ -40,19 +51,36 @@ int main(int argc, char *argv[]) {
   	return -1;
   }
 
+  // Parse the params
   bzero((char *) &recv_addr, sizeof(recv_addr));
   portno = atoi(argv[2]);
   address = argv[1];
-  recv_addr.sin_family = AF_INET;
-  recv_addr.sin_addr.s_addr = inet_addr(address);
-  recv_addr.sin_port = htons(portno);
+  file_path = argv[3];
 
-  if ( bind(sockfd, (struct sockaddr *) &recv_addr, sizeof(recv_addr)) < 0)
-  {
+  // Connect and bind
+  recv_addr = setAddressAndPortNo(address, portno);
+
+  if ( bind(sockfd, (struct sockaddr *) &recv_addr, sizeof(recv_addr)) < 0) {
   	close(sockfd);
-  	fprintf(stderr, "REC:ERROR: error on bind()\n" );
+  	fprintf(stderr, "REC: Error on error on bind()\n" );
   	return -1;
   }
+
+  // Open the file for writing
+  if ( checkFilePath(file_path) < 0 ) {
+    fprintf(stderr, "REC: Error- no file found to write too\n");
+  }
+
+  FILE *write_file = fopen(file_path, "w");
+  if (write_file == NULL) {
+      printf("REC: Error opening file!\n");
+      exit(1);
+  }
+
+  // TODO: place this better
+  char *text = "Write this to the file";
+  fprintf(write_file, "TEST: Some text: %s\n", text);
+  fclose(write_file); // TODO: Move me to the end
 
   // We are now connected and serving
   printf("rdpr is running on RDP port %i.\n", portno);
@@ -74,4 +102,54 @@ int main(int argc, char *argv[]) {
   }
 
 	return -1;
+}
+
+// void buildRDPHeader(char *header, int type, int seqnum, int acknum, int payloadlength, int winsize) {
+
+//   char *typeString = NULL;
+
+//   switch ( type ) {
+//     case DAT_TYPE:
+//       typeString = "DAT";
+//       break;
+//     case ACK_TYPE:
+//       typeString = "ACK";
+//       break;
+//     case SYN_TYPE:
+//       typeString = "SYN";
+//       break;
+//     case FIN_TYPE:
+//       typeString = "FIN";
+//       break;
+//     case RST_TYPE:
+//       typeString = "RST";
+//       break;
+//     default:
+//       typeString = "RST";
+//       break;
+//   }
+
+//   fprintf(header, "UVicCSc361 %s %i %i %i %i\n\n", seqnum, acknum, payloadlength, winsize);
+// }
+
+struct sockaddr_in setAddressAndPortNo(char *addr, int portno) {
+
+  struct sockaddr_in to;
+  to.sin_family = AF_INET;
+  to.sin_addr.s_addr = inet_addr(addr);
+  to.sin_port = htons(portno);  
+
+  return to;
+}
+
+int sendString(int sockfd, char *buffer, struct sockaddr_in addr, socklen_t len) {
+    if ((sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&addr, len)) == -1) {
+        perror("Error sending string to socket.");
+        return -1;
+    }   
+    return 1;
+}
+
+int checkFilePath(char *loc) {
+    return access(loc, F_OK);
 }
--- rdps.c
+++ rdps.c
@@ -17,7 +17,26 @@
 #include <errno.h>
 #include <time.h> 
 
+#define MAXPACKETSIZE 1024
 #define MAXBUFLEN 256
+
+#define DAT_TYPE 0
+#define ACK_TYPE 1
+#define SYN_TYPE 2
+#define FIN_TYPE 3
+#define RST_TYPE 4
+
+// Builds a packet to be sent over the wire
+void buildRDPHeader(char *header, int type, int seqnum, int acknum, int payloadlength, int winsize);
+
+// Set portNo on an addr
+struct sockaddr_in setAddressAndPortNo(char *addr, int portno);
+
+// Send a string to the client
+int sendString(int sockfd, char *buffer, struct sockaddr_in addr, socklen_t len);
+
+// Confirm the file exists
+int checkFilePath(char *loc);
 
 int main(int argc, char *argv[]) {
 	int sockfd;
@@ -25,9 +44,11 @@ int main(int argc, char *argv[]) {
 	int dest_portno;
 	char *address;
 	char *dest_address;
+  char *file_path;
 	struct sockaddr_in recv_addr, sender_addr;
   char buffer[MAXBUFLEN]; 
-  socklen_t cli_len, serv_len;
+  socklen_t recv_len, sender_len;
+
 
 	// Confirm command line args
   if ( argc != 6 ) {
@@ -42,48 +63,141 @@ int main(int argc, char *argv[]) {
   	return -1;
   }
 
+  // Parse the params
   bzero((char *) &recv_addr, sizeof(recv_addr));
   address = argv[1];
   dest_address = argv[3];
   portno = atoi(argv[2]);
   dest_portno = atoi(argv[4]);
+  file_path = argv[5];
 
-  recv_addr.sin_family = AF_INET;
-  recv_addr.sin_addr.s_addr = inet_addr(dest_address);
-  recv_addr.sin_port = htons(dest_portno);
+  // Connect and bind
+  recv_addr = setAddressAndPortNo(dest_address, dest_portno);
+  sender_addr = setAddressAndPortNo(address, portno);
 
-  sender_addr.sin_family = AF_INET;
-  sender_addr.sin_addr.s_addr = inet_addr(address);
-  sender_addr.sin_port = htons(portno);
+  if ( bind(sockfd, (struct sockaddr *) &sender_addr, sizeof(sender_addr)) < 0) {
+    close(sockfd);
+    fprintf(stderr, "SEN: Error error on bind()\n" );
+    return -1;
+  }
 
-  if ( bind(sockfd, (struct sockaddr *) &sender_addr, sizeof(sender_addr)) < 0)
-  {
-  	close(sockfd);
-  	fprintf(stderr, "SEN: Error error on bind()\n" );
-  	return -1;
+  recv_len = sizeof(recv_addr);
+  sender_len = sizeof(sender_addr);
+  
+  // Read file into buffer
+  if ( checkFilePath(file_path) < 0 ) {
+    fprintf(stderr, "SEN: Error- no file found to send\n");
+  }
+
+  char *payload = NULL;
+  FILE *fp = fopen(file_path, "r");
+  if (fp != NULL) {
+
+    if (fseek(fp, 0L, SEEK_END) == 0) {
+
+      long bufsize = ftell(fp);
+      if (bufsize == -1) { 
+        fprintf(stderr, "SEN: Error error on bufsize\n" );
+      }
+
+      payload = malloc(sizeof(char) * (bufsize + 1));
+
+      if (fseek(fp, 0L, SEEK_SET) != 0) { 
+        fprintf(stderr, "SEN: Error error fseek\n" );
+      }
+
+      size_t newLen = fread(payload, sizeof(char), bufsize, fp);
+      if (newLen == 0) {
+        fprintf(stderr, "SEN: Error reading file");
+      } else {
+        payload[++newLen] = '\0'; 
+      }
+    }
+    fclose(fp);
   }
 
   // We are now connected and serving
   printf("rdps is running on RDP port %i.\n", portno);
 
+  char header[MAXBUFLEN];
+  buildRDPHeader(header, DAT_TYPE, 10, 11, 12, 13);
+  printf("%s\n", header);
+
   // Running loop
   while ( 1 ) {
-    cli_len = sizeof(sender_addr);
-    serv_len = sizeof(recv_addr);
-
-    if ((sendto(sockfd, ">test<", strlen(">test<"), 0, (struct sockaddr *)&recv_addr, serv_len)) == -1) {
-        perror("SEN: Error sending string to socket.");
-        return -1;
-    }   
-
+    sendString(sockfd, "test<<", recv_addr, recv_len);
+  
     int numbytes;
     if ((numbytes = recvfrom(sockfd, buffer, MAXBUFLEN-1 , 0,
-        (struct sockaddr *)&sender_addr, &cli_len)) == -1) {
+        (struct sockaddr *)&recv_addr, &recv_len)) == -1) {
         perror("SEN: Error on recvfrom()!");
         return -1;
     }
 
   }
 
+  /*
+  Reciever initiates a file, thats where all its stored data is saved
+  Sender specifies a file to send to the reciever
+  */
+
+  /*
+  Sender intitiates by having the recievers IP the packets from then on containt where the IP:PoRTNO of the next packet will come from 
+  */
+
+  /*
+  Packets items and flags are seperated by a space 
+  */
+  free(payload);
 	return -1;
+}
+
+void buildRDPHeader(char *header, int type, int seqnum, int acknum, int payloadlength, int winsize) {
+
+  char *typeString = NULL;
+
+  switch ( type ) {
+    case DAT_TYPE:
+      typeString = "DAT";
+      break;
+    case ACK_TYPE:
+      typeString = "ACK";
+      break;
+    case SYN_TYPE:
+      typeString = "SYN";
+      break;
+    case FIN_TYPE:
+      typeString = "FIN";
+      break;
+    case RST_TYPE:
+      typeString = "RST";
+      break;
+    default:
+      typeString = "RST";
+      break;
+  }
+
+  sprintf(header, "UVicCSc361 %s %i %i %i %i\n\n", typeString, seqnum, acknum, payloadlength, winsize);
+}
+
+struct sockaddr_in setAddressAndPortNo(char *addr, int portno) {
+
+  struct sockaddr_in to;
+  to.sin_family = AF_INET;
+  to.sin_addr.s_addr = inet_addr(addr);
+  to.sin_port = htons(portno);  
+
+  return to;
+}
+
+int sendString(int sockfd, char *buffer, struct sockaddr_in addr, socklen_t len) {
+    if ((sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&addr, len)) == -1) {
+        perror("Error sending string to socket.");
+        return -1;
+    }   
+    return 1;
+}
+
+int checkFilePath(char *loc) {
+    return access(loc, F_OK);
 }
